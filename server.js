7a14410c00e509fb8edb033036243c82c1341037
Hillary <Ibeanuhillary@gmail.com>
2019-07-04 16:46:13 +0100

    update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//require('dotenv').config();
+require('dotenv').config();
 const port = process.env.PORT;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -8,17 +8,10 @@ const app = express();
 const productsRoute = require('./controllers/products');
 const CourseRoute = require('./controllers/course');
 
-
-
-
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', error => console.log(error));
 db.once('open', () => console.log('connected to database'));
-
-
-
-
 
 app.set('view engine', 'ejs');
 app.use(cors());//using cors for all api endpoint
